f196b6a6e483ee01942f55af80be4a5b7a00b8a7
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-11-17 19:07:15 -0300

    rentiki

--- rentiki_app/src/app/houses/houses.page.ts
+++ rentiki_app/src/app/houses/houses.page.ts
@@ -1,3 +1,4 @@
+import { HousesService } from './houses.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HousesPage implements OnInit {
 
-  constructor() {}
+  houses!: any;
+
+  constructor(private housesService: HousesService) {}
 
   ngOnInit() {
+    // this.housesService.fetchHouses().subscribe(houses => {
+    //   this.houses = houses
+    //   console.log(houses)
+    //   return houses
+    // })
+  }
+
+  onfetchHouses() {
+    this.housesService.fetchHouses().subscribe(houses => {
+      this.houses = houses
+      console.log(houses)
+      return houses
+    })
   }
 
   //Fazer fetch no backend e trazer os dados
--- rentiki_app/src/app/houses/houses.service.ts
+++ rentiki_app/src/app/houses/houses.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousesService {
 
-  constructor() { }
+  dataUrl = 'http://localhost:3001/users'
+
+  constructor(private http: HttpClient) { }
+
+  fetchHouses() {
+    return this.http.get<any>(this.dataUrl);     
+  }
+
 }
