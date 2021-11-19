9706ab61a770b2379c306129be6c9a088b0b2878
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-11-19 20:16:49 -0300

    rentiki

--- rentiki_app/src/app/houses/houses.module.ts
+++ rentiki_app/src/app/houses/houses.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SwiperModule } from 'swiper/angular';
@@ -6,6 +6,10 @@ import { IonicModule } from '@ionic/angular';
 import { HousesPageRoutingModule } from './houses-routing.module';
 import { HousesPage } from './houses.page';
 import { SlideComponent } from './slide/slide.component';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
 
 @NgModule({
   imports: [
@@ -15,6 +19,10 @@ import { SlideComponent } from './slide/slide.component';
     SwiperModule,
     HousesPageRoutingModule
   ],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   declarations: [HousesPage, SlideComponent]
 })
 export class HousesPageModule {}
