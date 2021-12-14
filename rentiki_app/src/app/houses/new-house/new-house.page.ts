5311ed6362ce52062728702313050d97f38eaba7
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-12-14 17:15:24 -0300

    rentiki

--- rentiki_app/src/app/houses/new-house/new-house.page.ts
+++ rentiki_app/src/app/houses/new-house/new-house.page.ts
@@ -3,6 +3,27 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+function base64toBlob(base64Data, contentType) {
+  contentType = contentType || '';
+  const sliceSize = 1024;
+  const byteCharacters = window.atob(base64Data);
+  const bytesLength = byteCharacters.length;
+  const slicesCount = Math.ceil(bytesLength / sliceSize);
+  const byteArrays = new Array(slicesCount);
+
+  for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+    const begin = sliceIndex * sliceSize;
+    const end = Math.min(begin + sliceSize, bytesLength);
+
+    const bytes = new Array(end - begin);
+    for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+      bytes[i] = byteCharacters[offset].charCodeAt(0);
+    }
+    byteArrays[sliceIndex] = new Uint8Array(bytes);
+  }
+  return new Blob(byteArrays, { type: contentType });
+}
+
 @Component({
   selector: 'app-new-house',
   templateUrl: './new-house.page.html',
@@ -87,5 +108,23 @@ export class NewHousePage implements OnInit {
         this.form.reset();
         this.router.navigateByUrl('/houses');
       });
+  }
+
+  onImagePicked(imageData: string | File) {
+    let imageFile;
+    if (typeof imageData === 'string') {
+      try {
+        imageFile = base64toBlob(
+          imageData.replace('data:image/jpeg;base64,', ''),
+          'image/jpeg'
+        );
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+    } else {
+      imageFile = imageData;
+    }
+    this.form.patchValue({ image: imageFile });
   }
 }
--- rentiki_app/src/app/shared/pickers/image-picker/image-picker.component.ts
+++ rentiki_app/src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Camera, CameraResultType } from '@capacitor/camera';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  Input,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-image-picker',
@@ -7,9 +17,64 @@ import { Camera, CameraResultType } from '@capacitor/camera';
   styleUrls: ['./image-picker.component.scss'],
 })
 export class ImagePickerComponent implements OnInit {
+  @ViewChild('filePicker', { static: false })
+  filePickerRef: ElementRef<HTMLInputElement>;
+  @Output() imagePick = new EventEmitter();
+  @Input() showPreview = false;
+  selectedImage: string;
+  usePicker = false;
 
-  constructor() { }
+  constructor(private platform: Platform) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (
+      (this.platform.is('mobile') && !this.platform.is('hybrid')) ||
+      this.platform.is('desktop')
+    ) {
+      this.usePicker = true;
+    }
+  }
 
+  onPickImage() {
+    if (!Capacitor.isPluginAvailable('Camera')) {
+      this.filePickerRef.nativeElement.click();
+      return;
+    }
+
+    Camera.getPhoto({
+      quality: 100,
+      source: CameraSource.Prompt,
+      correctOrientation: true,
+      resultType: CameraResultType.DataUrl,
+    })
+      .then((image) => {
+        this.selectedImage = image.dataUrl;
+        this.imagePick.emit(image.dataUrl);
+        if (this.selectedImage) {
+          this.showPreview = true;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        if (this.usePicker) {
+          this.filePickerRef.nativeElement.click();
+        }
+        return false;
+      });
+  }
+
+  onFileChosen(event: Event) {
+    const pickedFile = (event.target as HTMLInputElement).files[0];
+    if (!pickedFile) {
+      return;
+    }
+    const fr = new FileReader();
+    fr.onload = () => {
+      const dataUrl = fr.result.toString();
+      this.selectedImage = dataUrl;
+      this.imagePick.emit(pickedFile);
+      this.showPreview = true;
+    };
+    fr.readAsDataURL(pickedFile);
+  }
 }
